7a95020ea0effb8ea178e80e7e88c1f2e884f73a
Ayush Tripathi <atripathi@ucdavis.edu>
2024-08-09 14:33:52 -0700

    used streamlet's chatting experience for cleaner look

--- webapp.py
+++ webapp.py
@@ -1,39 +1,38 @@
 import streamlit as st
 from Chatbot import Chatbot
 
-chatter = Chatbot()
 
-# Initialize session state to store the conversation history
-if 'conversation' not in st.session_state:
-    st.session_state.conversation = []
 
 if 'input_text' not in st.session_state:
     st.session_state.input_text = ""
 
-def send_message():
-    user_input = st.session_state.input_text
+def send_pretty_message():
+    user_input = st.chat_input("Ask a question")
     if user_input:
-        # Append user input to conversation
-        st.session_state.conversation.append(f"You: {user_input}")
-        
-        # Get the chatbot response
+        with st.chat_message("User"):
+            st.markdown(user_input)
+        st.session_state.messages.append({"role": "User", "content": user_input})
+    
         response = chatter.chat(user_input)
-        st.session_state.conversation.append(f"Chatbot: {response}")
-        
-        # Clear the input box after sending
-        st.session_state.input_text = ""
+        with st.chat_message("Assistant"):
+            st.write(response)
+        st.session_state.messages.append({"role": "Assistant", "content": response})
+
 
 # Streamlit app layout
 st.title("iSteer Installation Assistant")
 
-# Display the conversation history
-for message in st.session_state.conversation:
-    st.write(message)
+chatter = Chatbot()
+ini_response = "Hello! I am here to assist you with any questions regarding Tibco EBX. I can answer specific installation questions while also assisting you in setting up Tibco on numerous platforms."
 
-# Text input for user prompt with submit button
-st.text_input('You: ', key='input_text')
-st.button('Send', on_click=send_message)
+# Initialize session state to store the conversation history
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+    with st.chat_message("Assistant"):
+        st.markdown(ini_response)
 
-# Allow clearing the conversation
-if st.button("Clear Chat"):
-    st.session_state.conversation = []
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+send_pretty_message()
